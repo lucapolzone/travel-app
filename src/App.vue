5469c8494a3a2fb87e3bcd6ff09bdec481e1449a
lucapolzone <lucapolzone@gmail.com>
2024-07-31 19:18:00 +0200

    feat: add calendar carousel

--- src/App.vue
+++ src/App.vue
@@ -8,32 +8,383 @@
     data() {
       return {
         // activeIndex: 0,
-        januaryDays: [
+        juneDays: [
           {
             dayNumber: 1,
-            dayName: "LUN",
+            dayName: "SAT",
           },
           {
             dayNumber: 2,
-            dayName: "MAR",
+            dayName: "SUN",
           },
           {
             dayNumber: 3,
-            dayName: "MER",
+            dayName: "MON",
           },
           {
             dayNumber: 4,
-            dayName: "GIO",
+            dayName: "TUE",
           },
           {
             dayNumber: 5,
-            dayName: "VEN",
+            dayName: "WED",
           },
           {
             dayNumber: 6,
-            dayName: "SAB",
+            dayName: "THU",
+          },
+          {
+            dayNumber: 7,
+            dayName: "FRI",
+          },
+          {
+            dayNumber: 8,
+            dayName: "SAT",
+          },
+          {
+            dayNumber: 9,
+            dayName: "SUN",
+          },
+          {
+            dayNumber: 10,
+            dayName: "MON",
+          },
+          {
+            dayNumber: 11,
+            dayName: "TUE",
+          },
+          {
+            dayNumber: 12,
+            dayName: "WED",
+          },
+          {
+            dayNumber: 13,
+            dayName: "THU",
+          },
+          {
+            dayNumber: 14,
+            dayName: "FRI",
+          },
+          {
+            dayNumber: 15,
+            dayName: "SAT",
+          },
+          {
+            dayNumber: 16,
+            dayName: "SUN",
+          },
+          {
+            dayNumber: 17,
+            dayName: "MON",
+          },
+          {
+            dayNumber: 18,
+            dayName: "TUE",
+          },
+          {
+            dayNumber: 19,
+            dayName: "WED",
+          },
+          {
+            dayNumber: 20,
+            dayName: "THU",
+          },
+          {
+            dayNumber: 21,
+            dayName: "FRI",
+          },
+          {
+            dayNumber: 22,
+            dayName: "SAT",
+          },
+          {
+            dayNumber: 23,
+            dayName: "SUN",
+          },
+          {
+            dayNumber: 24,
+            dayName: "MON",
+          },
+          {
+            dayNumber: 25,
+            dayName: "TUE",
+          },
+          {
+            dayNumber: 26,
+            dayName: "WED",
+          },
+          {
+            dayNumber: 27,
+            dayName: "THU",
+          },
+          {
+            dayNumber: 28,
+            dayName: "FRI",
+          },
+          {
+            dayNumber: 29,
+            dayName: "SAT",
+          },
+          {
+            dayNumber: 30,
+            dayName: "SUN",
+          },
+        ],
+
+        julyDays: [
+          {
+            dayNumber: 1,
+            dayName: "MON",
+          },
+          {
+            dayNumber: 2,
+            dayName: "TUE",
+          },
+          {
+            dayNumber: 3,
+            dayName: "WED",
+          },
+          {
+            dayNumber: 4,
+            dayName: "THU",
+          },
+          {
+            dayNumber: 5,
+            dayName: "FRI",
+          },
+          {
+            dayNumber: 6,
+            dayName: "SAT",
+          },
+          {
+            dayNumber: 7,
+            dayName: "SUN",
+          },
+          {
+            dayNumber: 8,
+            dayName: "MON",
+          },
+          {
+            dayNumber: 9,
+            dayName: "TUE",
+          },
+          {
+            dayNumber: 10,
+            dayName: "WED",
+          },
+          {
+            dayNumber: 11,
+            dayName: "THU",
+          },
+          {
+            dayNumber: 12,
+            dayName: "FRI",
+          },
+          {
+            dayNumber: 13,
+            dayName: "SAT",
+          },
+          {
+            dayNumber: 14,
+            dayName: "SUN",
+          },
+          {
+            dayNumber: 15,
+            dayName: "MON",
+          },
+          {
+            dayNumber: 16,
+            dayName: "TUE",
+          },
+          {
+            dayNumber: 17,
+            dayName: "WED",
+          },
+          {
+            dayNumber: 18,
+            dayName: "THU",
+          },
+          {
+            dayNumber: 19,
+            dayName: "FRI",
+          },
+          {
+            dayNumber: 20,
+            dayName: "SAT",
+          },
+          {
+            dayNumber: 21,
+            dayName: "SUN",
+          },
+          {
+            dayNumber: 22,
+            dayName: "MON",
+          },
+          {
+            dayNumber: 23,
+            dayName: "TUE",
+          },
+          {
+            dayNumber: 24,
+            dayName: "WED",
+          },
+          {
+            dayNumber: 25,
+            dayName: "THU",
+          },
+          {
+            dayNumber: 26,
+            dayName: "FRI",
+          },
+          {
+            dayNumber: 27,
+            dayName: "SAT",
+          },
+          {
+            dayNumber: 28,
+            dayName: "SUN",
+          },
+          {
+            dayNumber: 29,
+            dayName: "MON",
+          },
+          {
+            dayNumber: 30,
+            dayName: "TUE",
+          },
+          {
+            dayNumber: 31,
+            dayName: "WED",
+          },
+        ],
+
+        augustDays: [
+          {
+            dayNumber: 1,
+            dayName: "THU",
+          },
+          {
+            dayNumber: 2,
+            dayName: "FRI",
+          },
+          {
+            dayNumber: 3,
+            dayName: "SAT",
+          },
+          {
+            dayNumber: 4,
+            dayName: "SUN",
+          },
+          {
+            dayNumber: 5,
+            dayName: "MON",
+          },
+          {
+            dayNumber: 6,
+            dayName: "TUE",
+          },
+          {
+            dayNumber: 7,
+            dayName: "WED",
+          },
+          {
+            dayNumber: 8,
+            dayName: "THU",
+          },
+          {
+            dayNumber: 9,
+            dayName: "FRI",
+          },
+          {
+            dayNumber: 10,
+            dayName: "SAT",
+          },
+          {
+            dayNumber: 11,
+            dayName: "SUN",
+          },
+          {
+            dayNumber: 12,
+            dayName: "MON",
+          },
+          {
+            dayNumber: 13,
+            dayName: "TUE",
+          },
+          {
+            dayNumber: 14,
+            dayName: "WED",
+          },
+          {
+            dayNumber: 15,
+            dayName: "THU",
+          },
+          {
+            dayNumber: 16,
+            dayName: "FRI",
+          },
+          {
+            dayNumber: 17,
+            dayName: "SAT",
+          },
+          {
+            dayNumber: 18,
+            dayName: "SUN",
+          },
+          {
+            dayNumber: 19,
+            dayName: "MON",
+          },
+          {
+            dayNumber: 20,
+            dayName: "TUE",
+          },
+          {
+            dayNumber: 21,
+            dayName: "WED",
+          },
+          {
+            dayNumber: 22,
+            dayName: "THU",
+          },
+          {
+            dayNumber: 23,
+            dayName: "FRI",
+          },
+          {
+            dayNumber: 24,
+            dayName: "SAT",
+          },
+          {
+            dayNumber: 25,
+            dayName: "SUN",
+          },
+          {
+            dayNumber: 26,
+            dayName: "MON",
+          },
+          {
+            dayNumber: 27,
+            dayName: "TUE",
+          },
+          {
+            dayNumber: 28,
+            dayName: "WED",
+          },
+          {
+            dayNumber: 29,
+            dayName: "THU",
+          },
+          {
+            dayNumber: 30,
+            dayName: "FRI",
+          },
+          {
+            dayNumber: 31,
+            dayName: "SAT",
           },
         ]
+
       }
     },
 
@@ -47,7 +398,7 @@
     <h1>Travel App</h1>
 
     <app-travel-stages />
-    <app-calendar :januaryDays=januaryDays />
+    <app-calendar :juneDays="juneDays" :julyDays="julyDays" :augustDays="augustDays" />
     <app-map />
   
 </template>
--- src/components/AppCalendar.vue
+++ src/components/AppCalendar.vue
@@ -7,7 +7,9 @@
     },
 
     props: {
-      januaryDays: Array,
+      juneDays: Array,
+      julyDays: Array,
+      augustDays: Array,
     }
   }
   
@@ -15,24 +17,78 @@
 <template>
   <div class="container text-center">
 
-    <h2 class="mt-5 text-start">Calendario</h2>
+    <h2 class="mt-5">Calendario</h2>
 
-    <div class="row gy-2">
+    <div id="carouselExample" class="carousel carousel-dark slide">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="row gy-2">
+            <h3>Giugno</h3>
+            <div v-for="juneDay in juneDays" class="col-2">
+              <div class="card">
+                <p>
+                  {{ juneDay.dayNumber }}
+                </p>
+                <p>
+                  {{ juneDay.dayName }}
+                </p>
+              </div>
+            </div>
+          </div>     
+        </div>
+        <div class="carousel-item">
+          <div class="row gy-2">
+            <h3>Luglio</h3>
+            <div v-for="julyDay in julyDays" class="col-2">
+              <div class="card">
+                <p>
+                  {{ julyDay.dayNumber }}
+                </p>
+                <p>
+                  {{ julyDay.dayName }}
+                </p>
+              </div>
+            </div>
+          </div>   
+        </div>
+        <div class="carousel-item">
+          <div class="row gy-2">
+          <h3>Agosto</h3>
+           <div v-for="augustDay in augustDays" class="col">
+              <div class="card">
+                <p>
+                  {{ augustDay.dayNumber }}
+                </p>
+                <p>
+                  {{ augustDay.dayName }}
+                </p>
+              </div>
+            </div>
+          </div>
 
-      <div v-for="januaryDay in januaryDays" class="col">
-        <div class="card">
-          <p>
-            {{ januaryDay.dayNumber }}
-          </p>
-          <p>
-            {{ januaryDay.dayName }}
-          </p>
         </div>
       </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
 
-    </div>          
   </div>
 
 </template>
 <style lang="scss" scoped>
+  .carousel-item {
+    padding: 0 10rem;
+  }
+
+  .card {
+    width: 125px;
+    aspect-ratio: 1;
+    // height: 125px;
+  }
 </style>
