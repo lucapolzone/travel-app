5c89ad5a0d3f8a7ee9a35b2be4b1b211548da6e0
lucapolzone <lucapolzone@gmail.com>
2024-08-01 13:34:46 +0200

    feat: add back button

--- src/components/NewLocation.vue
+++ src/components/NewLocation.vue
@@ -10,23 +10,29 @@
   
 </script>
 <template>
-  <h2 class="text-center">Aggiungi tappa</h2>
   <div class="container">
-    <form>
-      <div class="mb-3">
-        <label for="dateLocation" class="form-label">Data tappa</label>
-        <input type="text" placeholder="01/06/2024" class="form-control" id="dateLocation" aria-describedby="dateHelp">
-      </div>
-      <div class="mb-3">
-        <label for="nameLocation" class="form-label">Tappa</label>
-        <input type="text" class="form-control" id="nameLocation">
-      </div>
-      <div class="mb-3">
-        <label for="descriptionLocation" class="form-label">Descrizione</label>
-        <textarea class="form-control" id="descriptionLocation" rows="4"></textarea>
-      </div>
-      <button type="submit" class="btn btn-success">INVIA</button>
-    </form>
+    <router-link to="/">
+      <button class="btn btn-warning">
+        <i class="fa-solid fa-arrow-left"></i> Indietro
+      </button>
+    </router-link>
+
+    <h2 class="text-center">Aggiungi tappa</h2>
+      <form>
+        <div class="mb-3">
+          <label for="dateLocation" class="form-label">Data tappa</label>
+          <input type="text" placeholder="01/06/2024" class="form-control" id="dateLocation" aria-describedby="dateHelp">
+        </div>
+        <div class="mb-3">
+          <label for="nameLocation" class="form-label">Tappa</label>
+          <input type="text" class="form-control" id="nameLocation">
+        </div>
+        <div class="mb-3">
+          <label for="descriptionLocation" class="form-label">Descrizione</label>
+          <textarea class="form-control" id="descriptionLocation" rows="4"></textarea>
+        </div>
+        <button type="submit" class="btn btn-success">INVIA</button>
+      </form>
   </div>
 </template>
 <style lang="scss" scoped>
