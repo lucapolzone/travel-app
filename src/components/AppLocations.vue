2f440736bd7ab256bc0f1fd14eeb7368e1d0a643
lucapolzone <lucapolzone@gmail.com>
2024-08-06 19:28:46 +0200

    feat: locations tables are now in a carousel

--- src/components/AppLocations.vue
+++ src/components/AppLocations.vue
@@ -32,98 +32,33 @@ export default {
 
   <div class="container">
 
-
     <h2 class="mt-5">Tappe viaggio</h2>
 
-    <table class="table table-bordered" v-for="locationGroup in locations">
-      <thead class="table-primary">
-        <tr>
-          <th scope="col" colspan="3" class="text-center">{{ locationGroup.date }}</th>
-        </tr>
-        <tr>
-          <th scope="col" colspan="3" class="text-center text-danger">{{ locationGroup.id }}</th>
-        </tr>
-        <tr>
-          <th scope="col" style="width: 200px;">Nome</th>
-          <th scope="col">Descrizione</th>
-        </tr>
-      </thead>      
-      <tbody>
-        <tr v-for="stage in locationGroup.stages">
-          <td>{{ stage.name }}</td>
-          <td>{{ stage.description }}</td>
-        </tr>
-      </tbody>
-    </table>
-    
-    <!-- 
     <div id="carouselLocations" class="carousel carousel-dark slide">
       <div class="carousel-inner">
-        <div class="carousel-item active">
-          <table class="table table-bordered mt-3">
+        <div
+          v-for="(locationGroup, index) in locations" 
+          :key="locationGroup.id" 
+          class="carousel-item" 
+          :class="{ active: index === 0 }"
+        >
+          <table class="table table-bordered">
             <thead class="table-primary">
               <tr>
-                <th colspan="5">01/06/2024</th>
+                <th scope="col" colspan="3" class="text-center">{{ locationGroup.date }}</th>
               </tr>
               <tr>
-                <th scope="col">Data</th>
-                <th scope="col">Nome</th>
-                <th scope="col">Descrizione</th>
-                <th scope="col">Modifica</th>
-                <th scope="col">Cancella</th>
+                <th scope="col" colspan="3" class="text-center text-danger">{{ locationGroup.id }}</th>
               </tr>
-            </thead>
-            <tbody>
               <tr>
-                <td>{{ locations[0].date }}</td>
-                <td>{{ locations[0].name }}</td>
-                <td>{{ locations[0].description }}</td>
-                <td>
-                  <a href="#" class="btn btn-warning">
-                    <i class="fa-solid fa-pen"></i>
-                  </a>
-                </td>
-                <td>
-                  <a href="#" class="btn btn-danger">
-                    <i class="fa-solid fa-trash-can"></i>
-                  </a>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div class="carousel-item">
-          <table class="table table-bordered mt-3">
-            <thead class="table-primary">
-              <tr>
-                <th scope="col">Data</th>
-                <th scope="col">Nome</th>
+                <th scope="col" style="width: 200px;">Nome</th>
                 <th scope="col">Descrizione</th>
               </tr>
-            </thead>
+            </thead>      
             <tbody>
-              <tr>
-                <td>{{ locations[1].date }}</td>
-                <td>{{ locations[1].name }}</td>
-                <td>{{ locations[1].description }}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div class="carousel-item">
-          <table class="table table-bordered mt-3">
-            <thead class="table-primary">
-              <tr>
-                <th scope="col">Data</th>
-                <th scope="col">Nome</th>
-                <th scope="col">Descrizione</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{{ locations[2].date }}</td>
-                <td>{{ locations[2].name }}</td>
-                <td>{{ locations[2].description }}</td>
+              <tr v-for="stage in locationGroup.stages">
+                <td>{{ stage.name }}</td>
+                <td>{{ stage.description }}</td>
               </tr>
             </tbody>
           </table>
@@ -138,7 +73,6 @@ export default {
         <span class="visually-hidden">Next</span>
       </button>
     </div>
-      -->
     
     
   </div>
