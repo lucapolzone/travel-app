751d1d14bf201d71eda619652e97317841e0d2ca
lucapolzone <lucapolzone@gmail.com>
2024-08-13 14:00:11 +0200

    refactor: reorganize files within folders

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import Home from './pages/Home.vue'; 
 import NewLocation from './pages/NewLocation.vue'; 
-import EditLocation from './components/EditLocation.vue';
+import EditLocation from './pages/EditLocation.vue';
 
 const router = createRouter({
   history: createWebHistory(),
